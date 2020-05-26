48d508eec54d55200d0311213f1f057d60db03ae
Cornelia Franta <franta-cornelia@gmx.at>
2020-05-26 10:56:04 +0200

    adding basic2.html and test.py

---
+++ test.py
@@ -0,0 +1,8 @@
+myArray = [1, 7, -3, 9, 3, 20, 8]
+highest_number = 0
+for i in myArray:
+    if i > highest_number:
+        highest_number = i
+        print(highest_number)
+
+print(highest_number)
